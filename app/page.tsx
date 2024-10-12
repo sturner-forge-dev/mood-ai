22a3ad6f683f6706a7df25550928e5563c371e06
Steven Turner <sturner@forge-applications.io>
2024-10-12 10:26:16 -0500

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">The best Jounal app, period.</h1>
+        <p className="text-2xl text-white/60 mb-4">
+          This is the best app for tracking your mood throughout your life. All
+          you have to do is be honest.
+        </p>
+        <div>
+          <Link href="/journal">
+            <button className="bg-blue-400 px-4 py-2 rounded-lg text-xl">
+              get started
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
